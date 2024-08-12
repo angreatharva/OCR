12ff322d43b6370918293ae2bcef1a3d096a085d
Atharva Angre <angreatharva08@gmail.com>
2024-08-12 18:21:46 +0530

    stream detection done

--- lib/IpCameraView.dart
+++ lib/IpCameraView.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
-import 'CameraController.dart';
-import 'InbuiltCameraView.dart';
 import 'OcrService.dart';
-import 'RtspService.dart';
+import 'CameraController.dart';
 import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'dart:typed_data';
 
 class IpCameraView extends StatefulWidget {
@@ -14,19 +13,16 @@ class IpCameraView extends StatefulWidget {
 }
 
 class _IpCameraViewState extends State<IpCameraView> {
-  final RtspService rtspService = RtspService();
   final OcrService ocrService = OcrService();
   final CameraController cameraController = Get.put(CameraController());
-
   VlcPlayerController? _vlcPlayerController;
-  Uint8List? _lastFrameBytes;
   bool _processing = false;
 
   @override
   void initState() {
     super.initState();
     initializePlayer();
-    startStreamingAndAnalysis();
+    startFrameAnalysis();
   }
 
   void initializePlayer() {
@@ -38,64 +34,38 @@ class _IpCameraViewState extends State<IpCameraView> {
     _vlcPlayerController = VlcPlayerController.network(
       rtspUrl,
       autoPlay: true,
-      options: VlcPlayerOptions(
-        /*advanced: VlcAdvancedOptions(
-            [
-          '--network-caching=100', // Lower caching to reduce latency
-          '--rtsp-tcp', // Use TCP, change to UDP if needed
-           ]
-        ),*/
-      ),
+      options: VlcPlayerOptions(),
     );
   }
 
-
-  Future<void> startStreamingAndAnalysis() async {
-    try {
-      await rtspService.startStreaming('rtsp://admin:admin@123@192.168.2.86/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif');
-
-      while (mounted) {
-        await Future.delayed(Duration(milliseconds: 100)); // Adjust delay as necessary
-
-        String framePath = '${await rtspService.getOutputDirectory()}/output_0001.png';
-        if (await File(framePath).exists()) {
-          final bytes = await File(framePath).readAsBytes();
-
-          if (_lastFrameBytes == null || _hasFrameChanged(bytes)) {
-            _lastFrameBytes = bytes;
-            await performOcr(framePath);
-          }
-        } else {
-          print("Frame not found at path: $framePath");
-        }
-      }
-    } catch (e) {
-      print("Error during streaming and OCR: $e");
-      cameraController.updateOcrResult("Error during streaming and OCR");
-    }
-  }
-  bool _hasFrameChanged(Uint8List currentFrameBytes) {
-    if (_lastFrameBytes == null) return true;
-    if (_lastFrameBytes!.length != currentFrameBytes.length) return true;
-
-    // Compare a sample of pixels (e.g., every 100th pixel)
-    for (int i = 0; i < _lastFrameBytes!.length; i += 10000) {
-      if (_lastFrameBytes![i] != currentFrameBytes[i]) {
-        return true;
-      }
-    }
-    return false;
+  void startFrameAnalysis() {
+    Future.delayed(Duration(seconds: 1), analyzeFrame);
   }
 
-  Future<void> performOcr(String framePath) async {
-    if (!_processing) {
+  Future<void> analyzeFrame() async {
+    if (_vlcPlayerController != null && !_processing) {
       _processing = true;
-
-      String ocrText = await ocrService.performOcr(framePath);
-      cameraController.updateOcrResult(ocrText);
-
+      Uint8List? snapshot = await _vlcPlayerController?.takeSnapshot();
+      if (snapshot != null) {
+        String filePath = await _saveImageToFile(snapshot);
+        await performOcr(filePath);
+      }
       _processing = false;
     }
+    startFrameAnalysis(); // Continue the loop
+  }
+
+  Future<String> _saveImageToFile(Uint8List imageBytes) async {
+    final tempDir = await getTemporaryDirectory();
+    final filePath = '${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.png';
+    final file = File(filePath);
+    await file.writeAsBytes(imageBytes);
+    return filePath;
+  }
+
+  Future<void> performOcr(String filePath) async {
+    String ocrText = await ocrService.performOcr(filePath);
+    cameraController.updateOcrResult(ocrText);
   }
 
   @override
@@ -103,14 +73,6 @@ class _IpCameraViewState extends State<IpCameraView> {
     return Scaffold(
       appBar: AppBar(
         title: Text('IP Camera with OCR'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.camera),
-            onPressed: () {
-              Get.to(() => InbuiltCameraView());
-            },
-          ),
-        ],
       ),
       body: Column(
         children: [
@@ -122,12 +84,6 @@ class _IpCameraViewState extends State<IpCameraView> {
               placeholder: Center(child: CircularProgressIndicator()),
             ),
           ),
-          ElevatedButton(
-            onPressed: () async {
-              startStreamingAndAnalysis();
-            },
-            child: Text('Start Streaming and OCR'),
-          ),
           Obx(() => Text('OCR Result: ${cameraController.ocrResult}')),
         ],
       ),
@@ -136,7 +92,6 @@ class _IpCameraViewState extends State<IpCameraView> {
 
   @override
   void dispose() {
-    rtspService.stopStreaming();
     ocrService.dispose();
     _vlcPlayerController?.dispose();
     super.dispose();
